bd2754ae0b9b47fd157d0bd8628b9191727e1992
AnibalH <robsdatasv@gmail.com>
2022-12-22 11:45:17 -0600

    day-3: Finished odd-or-even.

--- day_01-10/day-3/odd-or-even.py
+++ day_01-10/day-3/odd-or-even.py
@@ -2,3 +2,15 @@
 # Instructions
 
 # Write a program that works out whether if a given number is an odd or even number.
+
+
+
+# Print welcoming message
+
+user_number = int(input(("Please enter the number to evaluate: ---> ")))
+
+if user_number % 2 == 0:
+    print(f"The number {user_number} is even")
+else: 
+    print(f"The number {user_number} is odd")
+
