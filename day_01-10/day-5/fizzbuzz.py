6ea67d7c003049e1245ae331625683a54ff4ab9b
AnibalH <robsdatasv@gmail.com>
2022-12-26 14:29:27 -0600

    day-5: Completed Fizz Buzz project

---
+++ day_01-10/day-5/fizzbuzz.py
@@ -0,0 +1,20 @@
+# Instructions
+# You are going to write a program that automatically prints the solution to the FizzBuzz game.
+# Your program should print each number from 1 to 100 in turn.
+# When the number is divisible by 3 then instead of printing the number it should print "Fizz".
+# When the number is divisible by 5, then instead of printing the number it should print "Buzz".`
+#   And if the number is divisible by both 3 and 5 e.g. 15 then instead of the number it should print "FizzBuzz"
+
+fizz = "Fizz"
+buzz = "Buzz"
+
+
+for number in range (1, 101):
+    if ( number % 3 == 0 ) and ( number % 5 == 0 ):
+        print(fizz + " " + buzz)
+    elif ( number % 3 == 0 ):
+        print(fizz)
+    elif ( number % 5 == 0 ):
+        print(buzz)
+    else:
+        print(number)
