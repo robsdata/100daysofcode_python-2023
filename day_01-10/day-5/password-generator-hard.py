25b6a45024221756149af34995a51b177f585100
AnibalH <robsdatasv@gmail.com>
2022-12-26 16:49:09 -0600

    day-5: Start final project; password generator easy & hard

--- day_01-10/day-5/password-generator-hard.py
+++ day_01-10/day-5/password-generator-hard.py
@@ -9,6 +9,5 @@ nr_letters= int(input("How many letters would you like in your password?\n"))
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
 
-#Eazy Level - Order not randomised:
-#e.g. 4 letter, 2 symbol, 2 number = JduE&!91
-
+#Hard Level - Order of characters randomised:
+#e.g. 4 letter, 2 symbol, 2 number = g^2jk8&P
