61e91fd0eaab344b311d6f57610b4c8594658f20
AnibalH <robsdatasv@gmail.com>
2022-12-30 11:21:26 -0600

    day-7: Completed challenge3, starting challenge 4

---
+++ day_01-10/day-7/hangman-project/challenges/challenge3.py
@@ -0,0 +1,34 @@
+#Step 3
+
+import random
+word_list = ["aardvark", "baboon", "camel"]
+chosen_word = random.choice(word_list)
+word_length = len(chosen_word)
+
+#Testing code
+print(f'Pssst, the solution is {chosen_word}.')
+
+#Create blanks
+display = []
+for _ in range(word_length):
+    display += "_"
+
+#TODO-1: - Use a while loop to let the user guess again. The loop should only stop once the user has guessed all the letters in the chosen_word and 'display' has no more blanks ("_"). Then you can tell the user they've won.
+
+game_running = True
+
+while game_running == True:
+    guess = input("Guess a letter: ").lower()
+
+    #Check guessed letter
+    for position in range(word_length):
+        letter = chosen_word[position]
+        # print(f"Current position: {position}\n Current letter: {letter}\n Guessed letter: {guess}")
+        if letter == guess:
+            display[position] = letter
+
+    if not "_" in display:
+        game_running = False
+
+    print(display)
+    
---
+++ day_01-10/day-7/test.py
@@ -0,0 +1,9 @@
+
+display = [" ", " ", "a", "_"]
+
+print("_" in display)
+
+if not "_" in display:
+    print("if not")
+else: 
+    print("else")
